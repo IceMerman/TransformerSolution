cc1f9035ed756879d5c40cd953a1a1df2ef1b667
lzerokoo <jsjuanes51@gmail.com>
2017-07-15 14:53:19 -0500

    Added ternary in the verification of the test side

--- machines.py
+++ machines.py
@@ -80,16 +80,16 @@ class Transformer(object):
             Rm = 1e1000
         else:
             Rm = v / (i * math.cos(theta))
-        self.Rmtest = Rm * u.ternary(transform, a**2, 1)
-        self.Xmtest = Xm * u.ternary(transform, a**2, 1)
-        print("OC ternary: ", u.ternary(transform, a**2, 1))
+        self.Rmtest = Rm * u.ternary(transform, a**-2, 1)
+        self.Xmtest = Xm * u.ternary(transform, a**-2, 1)
+        print("OC ternary: ", u.ternary(transform, a**-2, 1))
         self.setZmtest()
         return True
 
     def ShortCircuitTest(self, v: float, i: float, w: float, transform: bool = False, a: float = 0):
         theta = math.acos(w / (v * i * math.sqrt(1)))
-        Zeqtest = (v / i) * u.ternary(transform, a**2, 1)
-        print("SC ternary: ", u.ternary(transform, a**2, 1))
+        Zeqtest = (v / i) * u.ternary(transform, a**-2, 1)
+        print("SC ternary: ", u.ternary(transform, a**-2, 1))
         Rserie = Zeqtest * math.cos(theta)
         Xserie = Zeqtest * math.sin(theta)
         self.Zeqserie = Rserie + 1j * Xserie
